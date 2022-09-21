92f7eeec8647b147912920f01850582cd5bdbe3b
unknown <jonathas.92santos@gmail.com>
2022-09-21 12:08:31 -0300

    Atualização run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 robot -d .logs -v BROWSER:headlesschrome Tests/Validar_Agi_Blog.robot
-=======
-robot -l NONE -o NONE -r NONE Tests/Validar_Agi_Blog.robot
-robot -d .logs Tests/Validar_Agi_Blog.robot
->>>>>>> 4314571676a150bf1c265748610006b50bd16983
